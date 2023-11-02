c2a0fea44202d81814dade522411f7fd8c06aa74
Sonicastra <Sonicastra@telenet.be>
2023-11-02 22:07:05 +0100

    Add navbar, add footer, add dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/content'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+export default defineNuxtConfig( {
+    devtools: { enabled: false },
+    css: [ '~/assets/css/main.css' ],
+    modules: [
+        '@nuxt/content',
+        'nuxt-icon'
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
-})
+} );
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="flex mb-16 md:mb-24 lg:mb-32">
+        <div class="prose lg:prose-xl text-white py-4 sm:py-6 lg:py-10">
+            <h1 class="font-merriweather text-4xl text-white underline decoration-sky-500">CodeWave</h1>
+            <p>Unlock WebDev Secrets: Learn, Create, Thrive!</p>
+            <p>Dive Into the Dynamic World of <span class="underline decoration-amber-400">Web Development</span>: Discover, Design, and Deploy Your Vision Online!</p>
+        </div>
+        <div class="border-2 border-amber-400 rounded-xl overflow-hidden">
+            <img src="~/assets/images/markus-spiske-unsplash.jpg" alt="graphity">
+        </div>
+    </section>
+
+    <section>
+        <h2 class="text-3xl text-white">Latest Blog Posts</h2>
+    </section>
+</template>
